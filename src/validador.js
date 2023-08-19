a28d6847b5ccc290e567e4757a9c88ae19758004
VitoriaMaciel0 <seu@email.com>
2023-08-19 00:07:33 -0300

    Primeiro commit

--- src/validador.js
+++ src/validador.js
@@ -1,44 +1,15 @@
-function validar_cep(cep) {
-  // Remover caracteres não numéricos do CEP
-  const cepLimpo = cep.replace(/\D/g, '');
+function validar_email(email) {
+  // Implemente sua validação de e-mail aqui
+}
 
-  // Verificar se o CEP possui 8 dígitos numéricos
-  if (cepLimpo.length === 8) {
-    return true; // CEP válido
-  } else {
-    return false; // CEP inválido
-  }
+function validar_cep(cep) {
+  // Implemente sua validação de CEP aqui
 }
 
 function validar_cpf(cpf) {
-  // Remover caracteres não numéricos do CPF
-  const cpfLimpo = cpf.replace(/\D/g, '');
-
-  // Verificar se o CPF possui 11 dígitos numéricos
-  if (cpfLimpo.length !== 11) {
-    return false; // CPF inválido
-  }
-
-  // Verificar se todos os dígitos são iguais (CPF inválido)
-  if (/^(\d)\1+$/.test(cpfLimpo)) {
-    return false; // CPF inválido
-  }
-
-  // Implementar a lógica de validação do CPF aqui
-  // ...
-
-  // Retornar true se o CPF for válido
-  return true;
+  // Implemente sua validação de CPF aqui
 }
 
 function validar_telefone(telefone) {
-  // Remover caracteres não numéricos do telefone
-  const telefoneLimpo = telefone.replace(/\D/g, '');
-
-  // Verificar se o telefone possui um formato válido
-  if (/^\d{10}$/.test(telefoneLimpo) || /^\d{11}$/.test(telefoneLimpo)) {
-    return true; // Telefone válido
-  } else {
-    return false; // Telefone inválido
-  }
+  // Implemente sua validação de telefone aqui
 }
